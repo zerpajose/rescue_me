cb503551ef12096248712cc0804eed7de07dc849
CryptoVincent <zjose88@gmail.com>
2022-07-22 03:15:32 -0400

    managing session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require('fs');
 require("dotenv").config({ path: ".env" });
+const cors = require('cors');
 
 const multer = require("multer");
 
@@ -10,19 +11,37 @@ const upload = multer({dest: 'uploads/'});
 const axios = require('axios');
 const FormData = require('form-data');
 
+const Web3Token = require('web3-token');
+
+const { ethers } = require("ethers");
+
+const { ANIMAL_CONTRACT_ADDRESS, ANIMAL_CONTRACT_ABI } = require("./constants/index");
+
 const PINATA_BEARER = process.env.PINATA_BEARER;
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
 const app = express();
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/upload_metadata", upload.single("photo"), uploadFile);
+app.use(cors());
+
+app.post("/animal", upload.single("photo"), uploadFile);
 
 async function uploadFile(req, res) {
 
-  const new_animal = req.body;
+  let token = req.cookies.token;
+  const { address, body } = await Web3Token.verify(token);
 
-  // console.log(Object.keys(req.file));
+  const contractOwner = await getOwner();
+
+  if(ethers.utils.getAddress(address) !== ethers.utils.getAddress(contractOwner)){
+    res.json({msg: "Not Contract Owner"});
+    res.end();
+  }
+
+  const new_animal = req.body;
 
   const readable = fs.createReadStream(req.file.path);
 
@@ -94,6 +113,38 @@ async function uploadFile(req, res) {
   res.end();
 }
 
-app.listen(3000, () => {
-  console.log(`Server started...`);
+app.post("/is_owner", isOwner);
+
+async function isOwner(req, res) {
+
+  const contractOwner = await getOwner();
+
+  const token = req.headers['authorization'];
+
+  const { address, body } = await Web3Token.verify(token);
+
+  if(ethers.utils.getAddress(address) === ethers.utils.getAddress(contractOwner)){
+
+    res.json({is_owner: true, token: token});
+  }
+  else{
+    res.json({is_owner: false, token: token});
+  }
+
+  res.end();
+}
+
+async function getOwner(){
+  
+  const customHttpProvider = new ethers.providers.JsonRpcProvider(ALCHEMY_API_KEY_URL);
+
+  const animalContract = new ethers.Contract(ANIMAL_CONTRACT_ADDRESS, ANIMAL_CONTRACT_ABI, customHttpProvider);
+
+  const contractOwner = await animalContract.owner();
+
+  return contractOwner;
+}
+
+app.listen(3001, () => {
+  console.log(`Server started at port 3001`);
 });
